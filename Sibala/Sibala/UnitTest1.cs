c35215a71b09bf1ed9fa8acb3589ba8f01cfa9fc
birdie <birdie.lin@titansoft.com.sg>
2024-09-22 22:11:38 +0800

    add cases for Player_With_All_The_Same_Kind, Player_With_No_Points, Player_With_One_Pair, Player_With_Two_Pair, When_Player_Win_By_All_The_Same_Kind, When_Player_Win_By_One_Pair

--- Sibala/Sibala/UnitTest1.cs
+++ Sibala/Sibala/UnitTest1.cs
@@ -1,4 +1,3 @@
-using System.Runtime.InteropServices;
 using FluentAssertions;
 using Sibala.Models;
 
@@ -10,10 +9,60 @@ public class Tests
     public void Setup()
     {
     }
+    
+    [TestCase("1111")]
+    [TestCase("2222")]
+    [TestCase("3333")]
+    public void Player_With_All_The_Same_Kind(string playerDice)
+    {
+        var player = new Player(playerDice);
+        
+        player.GetPoints();
+
+        player.Points.Should().Be(999);
+    }
+    
+    [TestCase("1234")]
+    [TestCase("1235")]
+    [TestCase("3222")]
+    [TestCase("3331")]
+    public void Player_With_No_Points(string playerDice)
+    {
+        var player = new Player(playerDice);
+        
+        player.GetPoints();
+
+        player.Points.Should().Be(0);
+    }
+
+    [TestCase ("2234", 7)]
+    [TestCase ("4435", 8)]
+    [TestCase ("1266", 3)]
+    public void Player_With_One_Pair(string playerDice, int expectedPoints)
+    {
+        var player = new Player(playerDice);
+        
+        player.GetPoints();
+
+        player.Points.Should().Be(expectedPoints);
+    }
+    
+    [TestCase ("2233", 6)]
+    [TestCase ("4455", 10)]
+    [TestCase ("1116", 0)]
+    public void Player_With_Two_Pair(string playerDice, int expectedPoints)
+    {
+        var player = new Player(playerDice);
+        
+        player.GetPoints();
+
+        player.Points.Should().Be(expectedPoints);
+    }
 
     [TestCase("1234", "1234", "Draw, No Points")]
     [TestCase("1114", "1115", "Draw, No Points")]
-    public void When_Two_Players_Are_No_Points_Should_Return_Draw(string player1Dice, string player2Dice, string expected)
+    [TestCase("1111", "2222", "Draw, No Points")]
+    public void When_Two_Players_Are_No_Points_Should_Return_Draw_No_Points(string player1Dice, string player2Dice, string expected)
     {
         var player1 = new Player(player1Dice);
         var player2 = new Player(player2Dice);
@@ -24,4 +73,37 @@ public class Tests
         actual.Should().BeEquivalentTo("Draw, No Points");
         
     }
+    
+    [TestCase("1111", "1234", "Player1 Win, All The Same Kind")]
+    [TestCase("1234", "1111", "Player2 Win, All The Same Kind")]
+    public void When_Player_Win_By_All_The_Same_Kind(string player1Dice, string player2Dice, string expected)
+    {
+        var player1 = new Player(player1Dice);
+        var player2 = new Player(player2Dice);
+        var game = new Game(player1, player2);
+        
+        var actual = game.GetResult();
+        
+        actual.Should().BeEquivalentTo(expected);
+    }
+    
+    [TestCase("3331", "2233", "Player1 Win, One Pair, Points: 6")]
+    [TestCase("1233", "4456", "Player2 Win, One Pair, Points: 11")]
+    [TestCase("3344", "5566", "Player2 Win, One Pair, Points: 12")]
+    public void When_Player_Win_By_One_Pair(string player1Dice, string player2Dice, string expected)
+    {
+        var player1 = new Player(player1Dice);
+        var player2 = new Player(player2Dice);
+        var game = new Game(player1, player2);
+        
+        var actual = game.GetResult();
+        
+        actual.Should().BeEquivalentTo(expected);
+    }
+
+    
+    
+    
+    
+    
 }
