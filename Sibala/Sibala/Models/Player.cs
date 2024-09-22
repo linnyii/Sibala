df96bca354acb886b02c091706e1b6db6515bcc7
birdie <birdie.lin@titansoft.com.sg>
2024-09-22 22:09:03 +0800

    add logic for GetPoints()

--- Sibala/Sibala/Models/Player.cs
+++ Sibala/Sibala/Models/Player.cs
@@ -9,13 +9,38 @@ public class Player
 
     public string Dice { get; set; }
     public int Points { get; set; }
-    public bool IsAllSameKind { get; set; }
+    public bool AllTheSameKind { get; set; }
 
     public void GetPoints()
     {
         if (Dice.Distinct().Count() == 4)
             Points = 0;
+        if (Dice.Distinct().Count() == 3)
+        {
+            var groups = Dice.GroupBy(p => p).ToList();
+            Points = groups.Where(g => g.Count() == 1).Sum(g => (int)char.GetNumericValue(g.Key));
+        }
+
         if (Dice.Distinct().Count() == 2)
-            Points = 0;
+        {
+
+            var groups = Dice.GroupBy(p => p).ToList();
+            if (groups.Any(g => g.Count() == 3))
+            {
+                Points = 0;
+            }
+
+            if (groups.All(g => g.Count() == 2))
+            {
+                Points = groups.Max(g => (int)char.GetNumericValue(g.Key)) * 2;
+            }
+        }
+
+        if (Dice.Distinct().Count() == 1)
+        {
+            Points = 999;
+            AllTheSameKind = true;
+        }
     }
+
 }
